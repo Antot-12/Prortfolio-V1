fd89a6b6d1b523ed7fb36c3550911fbfb5814118
Antot_12 <anton.shyrko@gmail.com>
2025-01-28 15:01:42 +0100

    change absolute path

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Anton Shyrko",
-  profile: '/profile.png',
+  profile: './profile.png',
   designation: "Full Stack Developer",
   description: "My name is Anton Shyrko. I am a passionate and motivated IT student currently pursuing my degree at the Technical University of Kosice. I specialize in full-stack development and love exploring new technologies. I enjoy problem-solving, learning, and contributing to innovative projects. I have experience in building and deploying web applications and am enthusiastic about creating impactful solutions for real-world problems.",
   email: 'antottoturial@gmail.com',
